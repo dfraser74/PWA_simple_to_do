6616bdcdc4ff815b363b6c3b915222b0c89ca0bf
viveksharma619 <s.vivekblog@gmail.com>
2017-03-02 01:16:15 +0530

    Added manifest.json

--- sw.js
+++ sw.js
@@ -1,2 +1,9 @@
 self.importScripts('node_modules/sw-toolbox/sw-toolbox.js'); // Update path to match your own setup
 self.toolbox.options.debug = true;
+
+self.toolbox.precache(['/node_modules/', '/css/', '/js/', '/assets/']);
+
+self.toolbox.router.get('/node_modules/(.*)', toolbox.cacheFirst);
+self.toolbox.router.get('/css/(.*)', toolbox.cacheFirst);
+self.toolbox.router.get('/js/(.*)', toolbox.cacheFirst);
+self.toolbox.router.get('/assets/(.*)', toolbox.cacheFirst);
