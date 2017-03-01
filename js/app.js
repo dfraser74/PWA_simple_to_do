365e3978dd62731a84e53cd40c3640067da40621
viveksharma619 <s.vivekblog@gmail.com>
2017-03-02 00:31:54 +0530

    Completed Todo App and registered SW

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var app = angular.module('app',['ngStorage']);
+
+app.controller('MainController',['$scope','$localStorage',function($scope, $localStorage){
+  $scope.list = [];
+  $scope.newItem = '';
+  if($localStorage.list){
+      $scope.list = $localStorage.list;
+    }
+  $scope.addToDo = function(){
+    if($scope.newItem !== ''){
+      $scope.list.push($scope.newItem);
+      $scope.newItem = '';
+      $localStorage.list = $scope.list;
+    }
+  }
+  $scope.remove = function(index){
+      $scope.list.splice(index,1);
+      $localStorage.list = $scope.list;
+  }
+}]);
---
+++ sw.js
@@ -0,0 +1,2 @@
+self.importScripts('node_modules/sw-toolbox/sw-toolbox.js'); // Update path to match your own setup
+self.toolbox.options.debug = true;
